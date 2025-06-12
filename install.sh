97142a9a65cbe36acba9e35b9ceb1d25c2575a8d
Volgine <71149051+Volgine@users.noreply.github.com>
2025-06-12 08:38:09 +0200

    Extend backend requirements

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@
 set -e
 sudo apt-get update
 sudo apt-get install -y tesseract-ocr poppler-utils
+
+# Install Python requirements for tests and source modules
 pip install -r requirements.txt
+# Install backend dependencies (also used by src/)
 pip install -r backend/requirements.txt
